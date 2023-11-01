374059f7bb7e5375d85d03b647c4bb82a3eff1e1
AnukruthiAtluri <148026179+AnukruthiAtluri@users.noreply.github.com>
2023-11-01 00:11:43 -0400

    Minor Config changes

--- src/ui-components/TodoCreateForm.d.ts
+++ src/ui-components/TodoCreateForm.d.ts
@@ -1,9 +1,3 @@
-/***************************************************************************
- * The contents of this file were generated with Amplify Studio.           *
- * Please refrain from making any modifications to this file.              *
- * Any changes to this file will be overwritten when running amplify pull. *
- **************************************************************************/
-
 import * as React from "react";
 import { GridProps, TextFieldProps } from "@aws-amplify/ui-react";
 export declare type EscapeHatchProps = {
